f3e22db648aab6b16c1d6461ac0a430ea158a028
Seo-Pt <seonah.pari.pt@gmail.com>
2022-02-25 16:29:16 +0000

    Added day-mode and night-mode buttons along with JavaScript. Added preview weather app img and styled

--- src/script.js
+++ src/script.js
@@ -1,1 +1,35 @@
-//           <img src="/images/weather/night-weather.png" class="img-fluid night-weather" alt="Weather App Night Mode"/>
+let nightModeButtonEl = document.querySelector("#night-mode-button");
+let dayModeButtonEl = document.querySelector("#day-mode-button");
+let weatherAppImgEl = document.querySelector("#weather-app-img");
+let weatherAppImgElHtml =undefined;
+
+function displayNightMode(){
+    weatherAppImgElHtml="";
+    weatherAppImgElHtml=`<img src="/images/weather/night-weather.png" class="img-fluid night-weather" alt="Weather App Night Mode"/>`;
+    weatherAppImgEl.innerHTML= weatherAppImgElHtml;
+
+    
+    nightModeButtonEl.classList.remove("enabled");
+    nightModeButtonEl.classList.add("disabled");
+
+    dayModeButtonEl.classList.remove("disabled");
+    dayModeButtonEl.classList.add("enabled");
+}
+
+nightModeButtonEl.addEventListener("click", displayNightMode)
+
+
+
+function displayDayMode(){
+    weatherAppImgElHtml="";
+    weatherAppImgElHtml=`<img src="/images/weather/day-weather.png" class="img-fluid day-weather mt-1 mb-4 rounded shadow" alt="Weather App Day Mode"/>`;
+    weatherAppImgEl.innerHTML= weatherAppImgElHtml;
+
+    dayModeButtonEl.classList.remove("enabled");
+    dayModeButtonEl.classList.add("disabled");
+
+    nightModeButtonEl.classList.remove("disabled");
+    nightModeButtonEl.classList.add("enabled");
+}
+
+dayModeButtonEl.addEventListener("click", displayDayMode);
