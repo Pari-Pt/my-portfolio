e54d97aa7fd79ce3f9b9339937d4c37e77b88879
Seo-Pt <seonah.pari.pt@gmail.com>
2022-02-25 16:57:25 +0000

    Changed font size for hero h1 in media query 390px max width

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
 let nightModeButtonEl = document.querySelector("#night-mode-button");
 let dayModeButtonEl = document.querySelector("#day-mode-button");
 let weatherAppImgEl = document.querySelector("#weather-app-img");
-let weatherAppImgElHtml =undefined;
+let weatherAppImgElHtml = "";
 
 function displayNightMode(){
     weatherAppImgElHtml="";
     weatherAppImgElHtml=`<img src="/images/weather/night-weather.png" class="img-fluid night-weather" alt="Weather App Night Mode"/>`;
     weatherAppImgEl.innerHTML= weatherAppImgElHtml;
-
+    
     
     nightModeButtonEl.classList.remove("enabled");
     nightModeButtonEl.classList.add("disabled");
-
+    
     dayModeButtonEl.classList.remove("disabled");
     dayModeButtonEl.classList.add("enabled");
 }
 
-nightModeButtonEl.addEventListener("click", displayNightMode)
+nightModeButtonEl.addEventListener("click", displayNightMode);
 
 
 
@@ -24,10 +24,10 @@ function displayDayMode(){
     weatherAppImgElHtml="";
     weatherAppImgElHtml=`<img src="/images/weather/day-weather.png" class="img-fluid day-weather mt-1 mb-4 rounded shadow" alt="Weather App Day Mode"/>`;
     weatherAppImgEl.innerHTML= weatherAppImgElHtml;
-
+    
     dayModeButtonEl.classList.remove("enabled");
     dayModeButtonEl.classList.add("disabled");
-
+    
     nightModeButtonEl.classList.remove("disabled");
     nightModeButtonEl.classList.add("enabled");
 }
